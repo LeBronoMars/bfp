29ebaec81235cecd0560fbac0ed4f71aa25f2a0c
lebronomars <lebronomars@gmail.com>
2016-06-02 10:38:16 +0800

    aaww

--- handlers/HandlerIncident.go
+++ handlers/HandlerIncident.go
@@ -63,28 +63,25 @@ func (handler IncidentHandler) Create(c *gin.Context) {
 		alarm_level := c.PostForm("alarm_level")
 		reported_by,_ := strconv.Atoi(c.PostForm("reported_by"))
 
-		result := handler.db.Create(&newIncident)
-		if result.RowsAffected > 0 {
-			incident_id := newIncident.Id
-			//create the very first fire status of incident
-			fireStatus := m.FireStatus{}
-			fireStatus.IncidentId = incident_id
-			fireStatus.Status = alarm_level
-			fireStatus.ReportedBy = reported_by
+		handler.db.Create(&newIncident)
+		
+		incident_id := newIncident.Id
+		//create the very first fire status of incident
+		fireStatus := m.FireStatus{}
+		fireStatus.IncidentId = incident_id
+		fireStatus.Status = alarm_level
+		fireStatus.ReportedBy = reported_by
 
-			handler.db.Create(&fireStatus)
-			qryIncident := m.FetchIncidents{}
-			incident := m.Incident{}
-			handler.db.Where("id = ?",incident_id).First(&incident)
-			statuses := []m.QryIncidents{}
-			handler.db.Where("incident_id = ?",incident_id).Order("fire_status_id desc").Find(&statuses)
-			qryIncident.Incident = incident
-			qryIncident.Status = statuses
-			c.JSON(http.StatusCreated,qryIncident)
-			return
-		} else {
-			respond(http.StatusBadRequest,result.Error.Error(),c,true)
-		}
+		handler.db.Create(&fireStatus)
+		qryIncident := m.FetchIncidents{}
+		incident := m.Incident{}
+		handler.db.Where("id = ?",incident_id).First(&incident)
+		statuses := []m.QryIncidents{}
+		handler.db.Where("incident_id = ?",incident_id).Order("fire_status_id desc").Find(&statuses)
+		qryIncident.Incident = incident
+		qryIncident.Status = statuses
+		c.JSON(http.StatusCreated,qryIncident)
+		return
 	} else {
 		respond(http.StatusForbidden,"Sorry, but your session has expired!",c,true)	
 	}
