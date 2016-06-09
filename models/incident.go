1503aec33dbcb03f83dc0334bc381417b6a12b63
LeBronoMars <lebronomars@gmail.com>
2016-06-09 23:37:57 +0800

    Added checking if gorm.DB is null in Incident model aftercreate callback

--- models/incident.go
+++ models/incident.go
@@ -21,13 +21,18 @@ func (i *Incident) AfterCreate(tx *gorm.DB) (err error) {
 	newUpdatedAt,err2 := time.ParseInLocation(time.RFC3339,i.UpdatedAt.Format(time.RFC3339),loc)
 	fmt.Printf("\n\nAFTER CREATE ---> %v\n",newCreatedAt)
 	if err1 == nil && err2 == nil {
-    	res1 := tx.Model(&i).Update("CreatedAt", newCreatedAt)
-    	res2 := tx.Model(&i).Update("UpdatedAt", newUpdatedAt)
-    	if res1 == nil && res2 == nil {
-    		fmt.Printf("\n\nSAVED!")
-    	}  else {
-    		fmt.Printf("\n ERROR IN PARSING DATE ---> %v\n\n",res1.Error.Error())
-    	}
+		if tx == nil {
+				fmt.Printf("\n\nTX IS NULL!")
+			} else {
+				fmt.Printf("\n\nTX NOT NULL!")
+				res1 := tx.Model(&i).Update("CreatedAt", newCreatedAt)
+		    	res2 := tx.Model(&i).Update("UpdatedAt", newUpdatedAt)
+		    	if res1 == nil && res2 == nil {
+		    		fmt.Printf("\n\nSAVED!")
+		    	}  else {
+		    		fmt.Printf("\n ERROR IN PARSING DATE ---> %v\n\n",res1.Error.Error())
+		    	}
+			}
 	} else {
 		fmt.Printf("\n ERROR IN PARSING DATE ---> %v\n\n",err1,err2)
 	}
