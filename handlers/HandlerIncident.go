00f4a8a7ab5efe8afc64a1bac95e90f4b3ca0f92
Russel <rsbulanon@dynamicobjx.com>
2016-06-09 20:12:41 +0800

    transferred

--- handlers/HandlerIncident.go
+++ handlers/HandlerIncident.go
@@ -75,10 +75,8 @@ func (handler IncidentHandler) Create(c *gin.Context) {
 		qryIncident := m.FetchIncidents{}
 		incident := m.Incident{}
 		handler.db.Where("id = ?",incident_id).First(&incident)
-		c.String(http.StatusCreated,"Created")
 		statuses := []m.QryIncidents{}
 		handler.db.Where("incident_id = ?",incident_id).Order("fire_status_id desc").Find(&statuses)
-		c.String(http.StatusCreated,"Created")
 		qryIncident.Incident = incident
 		qryIncident.Status = statuses
 		c.JSON(http.StatusCreated,qryIncident)
